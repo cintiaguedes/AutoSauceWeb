08d405494c64079877553f51736012042baea711
Cíntia <91399941+cintiaguedes@users.noreply.github.com>
2023-12-07 16:20:41 -0300

    "Add test cases for valid login, logout, and empty credentials in TestLogin class. Update LoginPage to handle new scenarios."

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,4 @@
 # pages/login_page.py
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -10,12 +9,30 @@ class LoginPage(BasePage):
     USERNAME_INPUT = (By.ID, "user-name")
     PASSWORD_INPUT = (By.ID, "password")
     LOGIN_BUTTON = (By.ID, "login-button")
+    LOGOUT_BUTTON = (By.ID, "logout-button")
     ERROR_MESSAGE = (By.CSS_SELECTOR, ".error-message-container")
 
     def login(self, username, password):
-        self.driver.find_element(*self.USERNAME_INPUT).send_keys(username)
-        self.driver.find_element(*self.PASSWORD_INPUT).send_keys(password)
-        self.driver.find_element(*self.LOGIN_BUTTON).click()
+        # Waiting for the username input field to be visible
+        username_input = self.wait_for_element_to_be_visible(self.USERNAME_INPUT)
+        username_input.send_keys(username)
+
+        # Waiting for the password input field to be visible
+        password_input = self.wait_for_element_to_be_visible(self.PASSWORD_INPUT)
+        password_input.send_keys(password)
+
+        # Waiting for the login button to be clickable and then clicking it
+        login_button = self.wait_for_element_to_be_clickable(self.LOGIN_BUTTON)
+        login_button.click()
+
+    def logout(self):
+        # Waiting for the logout button to be clickable and then clicking it
+        logout_button = self.wait_for_element_to_be_clickable(self.LOGOUT_BUTTON)
+        logout_button.click()
+
+    def is_logout_successful(self):
+        # Check if the login button is visible after logout, indicating successful logout
+        return self.is_element_visible(self.LOGIN_BUTTON)
 
     def get_error_message(self):
         return self.wait_for_element(*self.ERROR_MESSAGE).text
@@ -25,7 +42,7 @@ class LoginPage(BasePage):
             EC.visibility_of_element_located(locator)
         )
 
-    def login(self, username, password):
-        # Waiting for the username input field to be visible
-        username_input = self.wait_for_element_to_be_visible(self.USERNAME_INPUT)
-        username_input.send_keys(username)
+    def wait_for_element_to_be_clickable(self, locator, timeout=10):
+        return WebDriverWait(self.driver, timeout).until(
+            EC.element_to_be_clickable(locator)
+        )
--- tests/test_login.py
+++ tests/test_login.py
@@ -12,18 +12,35 @@ class TestLogin(unittest.TestCase):
     def test_valid_login(self):
         self.login_page.open("/login")
         self.login_page.login("standard_user", "secret_sauce")
-        # Add assertions for successful login
+        self.assertTrue(self.login_page.is_logout_successful())  # Assuming successful login leads to logout
 
     def test_invalid_login(self):
         self.login_page.open("/login")
         self.login_page.login("locked_out_user", "invalid_password")
         error_message = self.login_page.get_error_message()
-        self.assertEqual(error_message, "Epic sadface: Sorry, this user 
-has been locked out.")
+        self.assertEqual(error_message, "Epic sadface: Sorry, this user has been locked out.")
 
+    def test_logout(self):
+        # Assuming that the user is initially logged in
+        self.login_page.open("/dashboard")  # Change to the dashboard URL or wherever the user is redirected after login
+        self.login_page.logout()
+        self.assertTrue(self.login_page.is_logout_successful())
+
+    def test_empty_credentials(self):
+        self.login_page.open("/login")
+        self.login_page.login("", "")
+        error_message = self.login_page.get_error_message()
+        self.assertEqual(error_message, "Epic sadface: Username is required")
+        
     def tearDown(self):
         self.driver.quit()
 
 if __name__ == "__main__":
     unittest.main()
+
+#test_valid_login:  logs in with valid credentials and then checks if the logout is successful.
+
+#test_logout:  assumes the user is initially logged in, navigates to a dashboard (or any authenticated page), logs out, and checks if the logout is successful.
+
+#test_empty_credentials:  attempts to log in with empty credentials and checks if the appropriate error message is displayed.
 
